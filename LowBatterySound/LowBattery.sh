2b34c032938b46c44e85b57a81891106abf3b049
toran <toran@localhost.localdomain>
2017-07-16 12:51:57 +0530

    adding files

---
+++ LowBatterySound/LowBattery.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+    battery_level=`acpi -b |awk '{print ($(4)-0)}'`
+    status=`acpi -b |awk '{print ($(3))}'`   
+    #notify-send $battery_level
+    if [ $status != "Charging," ]
+    then
+        if [ $battery_level -lt "15" ]
+        then
+            mpg321 -frames 30 /media/toran/ExternalHDD/E/workspace/LinuxTweaks/LowBatterySound/gentex_cammander_3_code_3_horn-Brandon-938131891.mp3
+            sleep 2m
+        fi
+    fi
+done
+
