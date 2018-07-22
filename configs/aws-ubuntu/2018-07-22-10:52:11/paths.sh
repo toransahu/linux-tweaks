20fff6453d3267da0d59133259bc1bf350ee9b07
Toran Sahu <toran.sahu@yahoo.com>
2018-07-22 10:54:35 +0000

    [bak] added for aws_ubuntu

---
+++ configs/aws-ubuntu/2018-07-22-10:52:11/paths.sh
@@ -0,0 +1,38 @@
+#! /bin/sh
+#
+# paths.sh
+# Copyright (C) 2018 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+
+##
+# PATHs
+##
+
+# backup_confs.sh
+SOURCE_DIR="/home/toran"
+BAK_DIR="$(date +"%Y-%m-%d-%I:%M:%S")"
+
+if  [ "$(hostname)" = mint-ThinkPad-L440 ]; then
+
+    EROOT="/mnt/ExternalHDD/E"
+    WORKSPACE="$EROOT/workspace"
+    TARGET_DIR="$WORKSPACE/linux-tweaks/configs/mint/$BAK_DIR"
+    echo "All seems good. You're on computer: $(hostname)"
+elif [ "$(hostname)" = mint-ethereal ]; then
+    EROOT="/mnt/Other/toran/E"
+    WORKSPACE="$EROOT/workspace"
+    TARGET_DIR="$WORKSPACE/linux-tweaks/configs/mint/$BAK_DIR"
+    echo "All seems good. You're on computer: $(hostname)"
+elif [ "$(hostname)" = ip-172-31-24-111 ]; then
+	SOURCE_DIR="/home/ubuntu"
+    EROOT="/home/ubuntu"
+    WORKSPACE="$EROOT/workspace"
+    TARGET_DIR="$WORKSPACE/linux-tweaks/configs/aws-ubuntu/$BAK_DIR"
+    echo "All seems good. You're on computer: $(hostname)"
+else
+    echo "New device found. Make entry in the script.."
+fi
