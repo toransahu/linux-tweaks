891f74e4dcaaa8cd0d6f884cad087c27a90c78ec
Toran Sahu <toran.sahu@yahoo.com>
2018-08-03 16:28:26 +0530

    added windows ubuntu env in paths.sh

--- backups/paths.sh
+++ backups/paths.sh
@@ -35,17 +35,22 @@ elif [ "$(hostname)" = mint-ethereal ]; then
     TARGET_DIR="$WORKSPACE/$REPO_DIR/$BAK_DIR"
     echo "All seems good. You're on computer: $(hostname)"
 elif [ "$(hostname)" = ip-172-31-24-111 ]; then
-	SOURCE_DIR="/home/ubuntu"
+    SOURCE_DIR="/home/ubuntu"
     EROOT="/home/ubuntu"
     WORKSPACE="$EROOT/workspace"
     # TARGET_DIR="$WORKSPACE/linux-tweaks/configs/aws-ubuntu/$BAK_DIR"
     TARGET_DIR="$WORKSPACE/$REPO_DIR/$BAK_DIR"
     echo "All seems good. You're on computer: $(hostname)"    
+elif [ "$(hostname)" = DESKTOP-35VDAC5 ] && [ "$(uname)" = Linux ]; then
+    SOURCE_DIR="/home/toran"
+    EROOT="/mnt/g/toran/E"
+    WORKSPACE="$EROOT/workspace"
+    TARGET_DIR="$WORKSPACE/$REPO_DIR/$BAK_DIR"
+    echo "All seems good. You're on computer: $(hostname)"
 elif [ "$(hostname)" = DESKTOP-35VDAC5 ]; then
     SOURCE_DIR="/c/Users/Toran"
     EROOT="/g/toran/E"
     WORKSPACE="$EROOT/workspace"
-	# TARGET_DIR="$WORKSPACE/linux-tweaks/configs/windows"
     TARGET_DIR="$WORKSPACE/$REPO_DIR/$BAK_DIR"
     echo "All seems good. You're on computer: $(hostname)"
 elif [ "$(hostname)" = ip-172-31-18-37 ]; then
