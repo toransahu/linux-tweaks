f82ecb2dc29dc9f7e96b508660a2d4d5ec464b40
Toran Sahu <toran.sahu@yahoo.com>
2020-01-18 19:32:23 +0530

    more improvements

--- scripts/low_ram_action.sh
+++ scripts/low_ram_action.sh
@@ -8,6 +8,11 @@
 
 source ~/paths.sh
 
+TIMESTAMP=$(date +"%Y-%m-%d-%H:%M:%S")
+THRESHOLD_FIRST=10.0
+THRESHOLD_SECOND=5.0
+TICK=3
+
 free_ram () {
     sudo $WORKSPACE/linux-tweaks/scripts/freeram.sh
 }
@@ -16,36 +21,34 @@ kill_firefox () {
     APPS_TO_CLOSE='firefox'
     for app in $APPS_TO_CLOSE; do
         pids=$(pidof $app | awk '{$NF=""; print $0}')  # kill subtasks
-        # echo "Will kill $app[pids: $pids]"
+        echo "[$TIMESTAMP] Will kill $app[pids: $pids]"
         kill $pids
-        # echo "Killed $app[pids: $pids]"
+        echo "[$TIMESTAMP] Killed $app[pids: $pids]"
     done
 }
 
 run() {
     while true; do
         RAM_USGAE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-        RAM_FREE=$(free | grep Mem | awk '{print $4/$2 * 100.0}')
+        # RAM_FREE=$(free | grep Mem | awk '{print $4/$2 * 100.0}') # total - used - cahed - buffer
+        RAM_FREE=$(python -c "print(100 - $RAM_USGAE)") # total - used
         
         SWAP_USGAE=$(free | grep Swap | awk '{print $3/$2 * 100.0}')
         SWAP_FREE=$(free | grep Swap | awk '{print $4/$2 * 100.0}')
         
-        THRESHOLD_FIRST=15.0
-        THRESHOLD_SECOND=5.0
-        
+        echo "[$TIMESTAMP] Usage: $RAM_USGAE%, Free: $RAM_FREE%."
+
         if python -c "exit(0 if $RAM_FREE <= $THRESHOLD_FIRST else 1)"; then
-            # notify-send "Low Memory" "$RAM_FREE%. Freeing up some RAM."
-            # echo "[Low Memory] $RAM_FREE%. Freeing up some RAM."
+            echo Freeing up some RAM.
             free_ram
         fi
     
         if python -c "exit(0 if $RAM_FREE <= $THRESHOLD_SECOND else 1)"; then
-            # notify-send "Low Memory" "$RAM_FREE%. Killing firefox tabs."
-            # echo "[Very Low Memory] $RAM_FREE%. Killing firefox tabs."
+            echo Killing firefox tabs.
             kill_firefox
         fi
         
-        sleep 5;
+        sleep $TICK;
     done
 }
 
