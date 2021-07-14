6520c8e91a72cb59d5d9ba941b82bb90ae7ad47b
Toran Sahu <toran.sahu@yahoo.com>
2021-07-14 21:00:21 +0530

    /

---
+++ backups/.personalized/toran_all_startup_services.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# toran_all_startup_services.sh
+# Copyright (C) 2021 Toran Sahu <toran.sahu@yahoo.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+/usr/bin/barrierc --no-restart --name $HOST --enable-crypto $MY_IP_ADDR
